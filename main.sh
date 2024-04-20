cb834c3df6735bac868461656cf21f3f91b45f1f
d-rens <d-rens@mailbox.org>
2024-04-20 08:10:14 +0200

    Added new functions

--- main.sh
+++ main.sh
@@ -1,20 +1,17 @@
 #!/bin/sh
-#
-# main.sh c, calls the name feature via dmenu and then inserts the TeX 
-# code for the figure with the given name and filename.
-#
-# the case function will be removed once it is no longer needed for debugging
-#
 
 name=$(dmenu -p "Input the figure name here")
+dir="$HOME/lecture-notes/assets"
+tex_dir="~/lecture-notes/assets"
 key="$1"
+
 
 figure_code() {
     echo "Value of \$name: $name"
 
     line1="\\begin{figure}"
     line2="\\centering"
-    line3="\\includegraphics[width=\\linewidth]{$name.png}"
+    line3="\\includegraphics[width=\\linewidth]{assets/$name.png}"
     line4="\\caption{$name}"
     line5="\\end{figure}"
 
@@ -26,11 +23,39 @@ figure_code() {
     echo "$line4" >> "$temp_file"
     echo "$line5" >> "$temp_file"
 
+    xdotool type "i"
     xdotool type --file "$temp_file"
 
     rm "$temp_file"
 }
 
+
+open_xournalpp () {
+    cd $dir; 
+    if [ -f "$name.xopp" ]; then
+        cp temp.xopp $name.XXX.xopp;
+        xournalpp $name.xopp;
+    else 
+        cp temp.xopp $name.xopp;
+        xournalpp $name.xopp;
+    fi
+}
+
+
+convert () {
+    cd $dir;
+    xournalpp $name.xopp -i $name.png
+}
+
+all() {
+    figure_code &
+    open_xournalpp &
+    convert
+}
+
 case $key in 
-    c ) figure_code
+    f ) figure_code ;;
+    o ) open_xournalpp ;;
+    c ) convert ;;
+    a ) all ;;
 esac
